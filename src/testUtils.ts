c8849130f43593c0ec7f78d3057a77197b1e34fd
Shidil Eringa <shidil@live.com>
2020-08-07 19:22:34 +0530

    Created test utility exports

---
+++ src/testUtils.ts
@@ -0,0 +1,26 @@
+import { TableConfig } from './types';
+
+export const testTableConf: TableConfig = {
+  name: 'sample-table',
+  indexes: {
+    default: {
+      partitionKeyName: 'pk',
+      sortKeyName: 'sk',
+    },
+    reverse: {
+      partitionKeyName: 'sk',
+      sortKeyName: 'pk',
+    },
+  },
+};
+export const testClient = {
+  query: jest.fn().mockReturnThis(),
+  scan: jest.fn().mockReturnThis(),
+  get: jest.fn().mockReturnThis(),
+  put: jest.fn().mockReturnThis(),
+  write: jest.fn().mockReturnThis(),
+  batchGet: jest.fn().mockReturnThis(),
+  batchWrite: jest.fn().mockReturnThis(),
+  transactWrite: jest.fn().mockReturnThis(),
+  promise: jest.fn(),
+};
